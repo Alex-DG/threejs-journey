47a6ec477a4b1d5d9639fdc8b63ce1a61851e30d
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-05-02 16:28:04 +0100

    Lesson 20: Physics, add box, and sphere controls with GUI

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -8,6 +8,7 @@ import CANNON from 'cannon'
  * Debug
  */
 const gui = new dat.GUI()
+const debugObject = {}
 
 /**
  * Base
@@ -174,6 +175,105 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Utils
+ */
+const objectsToUpdate = []
+
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+})
+
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+  mesh.castShadow = true
+  mesh.scale.set(radius, radius, radius)
+  mesh.position.copy(position)
+  scene.add(mesh)
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius)
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: shape,
+    material: defaultMaterial,
+  })
+  body.position.copy(position)
+  world.addBody(body)
+
+  // Save in objects to update
+  objectsToUpdate.push({
+    mesh: mesh,
+    body: body,
+  })
+}
+
+// Create box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+})
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+  mesh.scale.set(width, height, depth)
+  mesh.castShadow = true
+  mesh.position.copy(position)
+  scene.add(mesh)
+
+  // Cannon.js body
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  )
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: shape,
+    material: defaultMaterial,
+  })
+  body.position.copy(position)
+  world.addBody(body)
+
+  // Save in objects
+  objectsToUpdate.push({ mesh, body })
+}
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 })
+
+/**
+ * GUI
+ */
+// debugObject.createSphere = () => {
+//   createSphere(0.5, { x: 0, y: 3, z: 0 })
+// }
+debugObject.createSphere = () => {
+  createSphere(Math.random() * 0.5, {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  })
+}
+
+gui.add(debugObject, 'createSphere')
+
+debugObject.createBox = () => {
+  createBox(Math.random(), Math.random(), Math.random(), {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  })
+}
+gui.add(debugObject, 'createBox')
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -185,9 +285,15 @@ const tick = () => {
   oldElapsedTime = elapsedTime
 
   // Update physics world
+
   sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position) // like a wind for applied on the ball at the end of the animation
 
   world.step(1 / 60, deltaTime, 3)
+
+  for (const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position)
+    object.mesh.quaternion.copy(object.body.quaternion)
+  }
 
   sphere.position.copy(sphereBody.position)
 
