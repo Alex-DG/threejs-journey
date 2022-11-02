3bbd81b4e0ea542089fce8d938337b17977f8faa
Alex-DG <alexandre.diguida@gmail.com>
2022-11-02 12:29:05 +0900

    Lesson 53: Fix bottom bound rigidity

--- 53-create-a-game-with-r3f/src/Level.js
+++ 53-create-a-game-with-r3f/src/Level.js
@@ -201,7 +201,6 @@ function Bounds({ length = 1 }) {
           scale={[0.3, 1.5, 4 * length]}
           castShadow
         />
-
         {/* Left */}
         <mesh
           position={[-2.15, 0.75, -(length * 2) + 2]}
@@ -210,18 +209,17 @@ function Bounds({ length = 1 }) {
           scale={[0.3, 1.5, 4 * length]}
           receiveShadow
         />
-
         {/* Bottom */}
         <mesh
           position={[0, 0.75, -(length * 4) + 2]}
           geometry={boxGeometry}
           material={wallMaterial}
-          scale={[4.6, 1.5, 0.3]}
-          castShadow
+          scale={[4, 1.5, 0.3]}
+          receiveShadow
         />
-
         {/* Floor */}
         <CuboidCollider
+          type="fixed"
           args={[2, 0.1, 2 * length]}
           position={[0, -0.1, -(length * 2) + 2]}
           restitution={0.2}
